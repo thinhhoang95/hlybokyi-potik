daf3a06260760ca7cd9119ace681483437c96b43
Thinh Hoang <dinhthinh.hoang@nxp.com>
2024-12-05 00:58:34 +0100

    I dont know what I did

--- path_prefix.py
+++ path_prefix.py
@@ -1,6 +1,6 @@
 # PATH_PREFIX = '/Users/thinhhoang/Documents/hlybokyi-potik' # Macbook Pro
 PATH_PREFIX = '/Volumes/CrucialX/hlybokyi-potik' # External SSD
-PATH_PREFIX = 'D:/hlybokyi-potik' # Windows
+# PATH_PREFIX = 'D:/hlybokyi-potik' # Windows
 PATH_PREFIX_DATA = PATH_PREFIX + '/data'
 
 # Folders under data
